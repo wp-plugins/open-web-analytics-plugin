374963c04e29875a7964893c7fda4252af767e8d
chrische <chrische@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-27 12:59:46 +0000

    renamed plugin to the official repository name, fixing nameing issues with openwebanalytics official
    git-svn-id: https://plugins.svn.wordpress.org/open-web-analytics-plugin/trunk@352436 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- openwebanalyticsplugin.php
+++ openwebanalyticsplugin.php
@@ -4,7 +4,7 @@ Plugin Name: Open Web Analytics
 Plugin URI: http://www.chrische.de/open-web-analytics-wordpress-plugin
 Description: This plugin makes it simple to add the Open Web Analytics tracking-code to your wordpress. <a href="options-general.php?page=openwebanalytics.php">Configuration Page</a>
 Author: Christian Schmidt
-Version: 1.1
+Version: 1.2
 Author URI: http://www.chrische.de/
 License: GPL
 
